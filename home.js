57b685f91e3652328e1dde0961ada6ec6c373081
Folareen <sakawahab03@gmail.com>
2022-05-14 17:26:10 +0100

    adjusted game bricks grids

---
+++ home.js
@@ -0,0 +1,5 @@
+const PLAY_PAUSE_BTN = document.querySelector('.play-pause');
+const STOP_BTN = document.querySelector('.stop');
+const SCORE = document.querySelector('.score');
+const GAME_STATUS = document.querySelector('.game-status');
+const GAME_CONTAINER = document.querySelector('.game') 
